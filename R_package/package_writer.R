fe4c0f0066fb0051b3eeadc851249f965a360f1b
Ziwei Zhu <zzw9348ustc@gmail.com>
2020-08-04 01:15:56 -0400

    change the package name to avoid confusion

--- R_package/package_writer.R
+++ R_package/package_writer.R
@@ -3,8 +3,10 @@
 library(devtools)
 library(roxygen2)
 setwd("/Users/ziweizhu/Dropbox/BambooLab_Templates/R_package")
-devtools::create("primePCA")
-setwd("/Users/ziweizhu/Dropbox/BambooLab_Templates/R_package/primePCA") # In case the path is not switched to the package
+devtools::create("primePCA.test")
+setwd("/Users/ziweizhu/Dropbox/BambooLab_Templates/R_package/primePCA.test") # In case the path is not switched to the package
+# Please modify the DESCRIPTION file if your new package depends on other
+# packages. Refer to the current DESCRIPTION file for details.  
 devtools::document()
 setwd("..")
-devtools::install("primePCA")
+devtools::install("primePCA_test")
